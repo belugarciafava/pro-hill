16103e3044b7eb071dd794b35751dce5f9dce355
Belu Garcia Fava <garciafava@gmail.com>
2018-08-15 18:16:04 -0300

    New pages

---
+++ src/main/java/com/eaprego/prohill/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.eaprego.prohill.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class UserController {
+
+    // inject via application.properties
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    @GetMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
