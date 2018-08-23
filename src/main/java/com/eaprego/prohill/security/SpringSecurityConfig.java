b25f2d36cbfc0105224a595accb1e2e1e2888b10
Belu Garcia Fava <garciafava@gmail.com>
2018-08-23 12:19:27 -0300

    Changes spring sec

--- src/main/java/com/eaprego/prohill/security/SpringSecurityConfig.java
+++ src/main/java/com/eaprego/prohill/security/SpringSecurityConfig.java
@@ -21,8 +21,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable()
                 .authorizeRequests()
-                .anyRequest().authenticated()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/pro-hill/**").permitAll()
                 .antMatchers("/admin/**").hasAnyRole("ADMIN")
                 .antMatchers("/**").hasAnyRole("USER")
                 .and()
